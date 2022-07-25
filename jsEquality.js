a90d937226cd087c535a926a3e4d42636dc643d2
Alexandru Teodorovici <teodorovici@gmail.com>
2022-07-25 10:56:47 +0300

    fdsafdsfsd

--- jsEquality.js
+++ jsEquality.js
@@ -1,1 +1,49 @@
 // JavaScript string to boolean
+
+
+
+// Method 1: Using the comparison operator
+// In Javascript, the comparison operator is a double equal sign 
+// whhich compares the values of the operands on the right and left sides.
+// It returns true if both operands have the same value and false otherwise.
+let stringValue = "true";
+let result = stringValue == "true"; //returns true 
+console.log("result is: " + result);
+
+let result2 = stringValue == true;
+console.log("result2 is: " + result2); //returns false
+
+// Method 4 : Using ternary operator
+
+// Method 3 : Using JSON.parse()
+let stringValue = "true"; 
+let boolValue = JSON.parse(stringValue); //returns true
+console.log("result2 is: " + result2); //returns false
+
+// Method 1 : Using test()
+
+
+// Method 5 : Using switch-case
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
